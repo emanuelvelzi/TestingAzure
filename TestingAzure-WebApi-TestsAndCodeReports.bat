b268d617e9f6209cb2d6606aeacca27e383da2a9
emanuel.velzi <emanuel.velzi@globallogic.com>
2018-08-14 13:50:14 -0300

    Improve tests

---
+++ TestingAzure-WebApi-TestsAndCodeReports.bat
@@ -0,0 +1,43 @@
+ REM Path variables 
+ setlocal
+ cd /d %~dp0
+ SET FolderPath=%cd%
+ SET ResultsPath=%FolderPath%\UnitTestResults
+ SET CoverageHistoryPath=%FolderPath%\CoverageHistory
+ 
+ SET NunitPath=%FolderPath%\packages\NUnit.ConsoleRunner.3.8.0\tools
+ SET ReportUnitPath=%FolderPath%\packages\ReportUnit.1.2.1\tools
+ SET OpenCoverPath=%FolderPath%\packages\OpenCover.4.6.519\tools
+ SET ReportGeneratorPath=%FolderPath%\packages\ReportGenerator.3.1.2\tools
+ SET ConverterCoverToCobertura=%FolderPath%\packages\OpenCoverToCoberturaConverter.0.2.6.0\tools
+ 
+ SET UnitTestProj=%FolderPath%\TestingAzure.Tests.WebApi\bin\Debug\TestingAzure.Tests.WebApi.dll
+ 
+ REM Recreate Results Folder
+ rd /S /Q %ResultsPath%
+ md %ResultsPath%
+ 
+ REM Create coverage history folder if not exists
+ if not exist "%CoverageHistoryPath%" mkdir %CoverageHistoryPath%
+ 
+ REM Run Nunit3 for tests, it produces TestResult.xml report
+ %NunitPath%\nunit3-console.exe %UnitTestProj% --result=%ResultsPath%\TestResult.xml
+ 
+ REM Get nunit command errorlevel
+ SET NunitError=%ERRORLEVEL%
+ 
+ REM Run ReportUnit to create HTML Report from Nunit XML report
+ %ReportUnitPath%\ReportUnit.exe %ResultsPath%\TestResult.xml
+ 
+ REM Run OpenCover to create coverage XML report
+ %OpenCoverPath%\OpenCover.Console.exe -register:user -register:user -target:%NunitPath%\nunit3-console.exe -targetargs:%UnitTestProj% -filter:"+[*]* -[FluentNHibernate*]*" -output:%ResultsPath%\opencovertests.xml
+ 
+ REM Run ReportGenerator to create coverage HTML report from coverage XML
+ %ReportGeneratorPath%\ReportGenerator.exe -reports:%ResultsPath%\opencovertests.xml -targetDir:%ResultsPath% -historydir:%CoverageHistoryPath%
+ 
+ %ConverterCoverToCobertura%\OpenCoverToCoberturaConverter.exe -input:%ResultsPath%\opencovertests.xml -output:%ResultsPath%\Cobertura.xml -sources:%ResultsPath%
+ 
+ 
+ REM Fail if Nunit has found an error on tests
+ exit /b %NunitError%
+ pause
---
+++ TestingAzure.Tests.WebApi/StadiumControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestingAzure.DataAccess.Interfaces;
+using TestingAzure.Entities;
+using TestingAzure.Tests.Common;
+using TestingAzure.WebApi.Controllers;
+
+namespace TestingAzure.Tests.WebApi
+{
+    public class StadiumControllerTest
+    {
+        private List<Stadium> StadiumsForTests;
+        private StadiumController StadiumController;
+
+        [SetUp]
+        public void Init()
+        {
+            this.StadiumsForTests = new List<Stadium> {
+                new Stadium {
+                    Id = 1,
+                    Capacity = 10000,
+                    City = "La Plata",
+                    Country = "Argentina" ,
+                    Description = "",
+                    Name = "Unico"
+                }
+            };
+            IRepository<Stadium> repo = new RepositoryStadiumMock(this.StadiumsForTests);
+            IUnitOfWork uow = new UnitOfWorkMock(repo);
+
+            this.StadiumController = new StadiumController(uow);
+        }
+
+        [Test]
+        public void GetStadium()
+        {
+            var stadiumsFromController = this.StadiumController.GetStadium();
+
+            Assert.AreEqual(stadiumsFromController, this.StadiumsForTests);
+        }
+    }
+}
